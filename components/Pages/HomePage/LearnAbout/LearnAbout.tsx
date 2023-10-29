78a908b176d33fe2a97f9163addcd03a3087b2f2
N Cee Dee <nnajidavid58@gmail.com>
2023-10-29 01:41:29 +0100

    started work on the home page

--- components/Pages/HomePage/LearnAbout/LearnAbout.tsx
+++ components/Pages/HomePage/LearnAbout/LearnAbout.tsx
@@ -5,14 +5,15 @@ import React from "react";
 
 export const LearnAbout = () => {
   return (
-    <div className="bg-primary text-accent flex justify-center sm:flex-col md:flex-row  md:space-x-4 sm:space-x-0 sm:space-y-4 p-4 mt-12">
+    <div className="bg-primary text-accent flex justify-center sm:flex-col md:flex-row  md:space-x-4 sm:space-x-0 sm:space-y-4 p-8 mt-12">
       <div className="md:w-[40%] sm:w-full">
         <Image
           src="/image/learnabout.jpeg"
           alt=""
-          width={500}
-          height={400}
-          className="rounded"
+          width={400}
+          height={300}
+          className="rounded w-full"
+          objectFit="cover"
         />
       </div>
       <div className="md:w-[40%] sm:w-full">
