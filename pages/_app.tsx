3b01324a0e1696d72e2c829899894cdef8d03276
N Cee Dee <nnajidavid58@gmail.com>
2023-10-29 15:13:04 +0100

    adde3d footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <div className="max-w-[2000px] m-auto">
             <Component {...pageProps} />
           </div>
         </main>
