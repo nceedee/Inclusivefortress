25711089ac335bb4900cca7cfbb18d385dd3837a
N Cee Dee <nnajidavid58@gmail.com>
2023-11-04 18:47:15 +0100

    updated

--- components/Pages/MissionAndVisionPage/MissionAndVisionPage.tsx
+++ components/Pages/MissionAndVisionPage/MissionAndVisionPage.tsx
@@ -9,16 +9,16 @@ export const MissionAndVisionPage = () => {
       <MissionAndVisionBackgroundImage />
       <div className="flex justify-around items-center bg-primary my-11 p-8 sm:flex-col md:flex-row  sm:space-y-5">
         <div className="md:w-[40%] sm:w-[100%]">
-          <h1 className="font-bold text-4xl text-center p-4 text-accent">
-            Our Mission Statement
-          </h1>
-          <Mission />
-        </div>
-        <div className="md:w-[40%] sm:w-[100%]">
           <h1 className="font-bold text-4xl text-accent text-center p-4">
             Our Vision Statement
           </h1>
           <Vision />
+        </div>
+        <div className="md:w-[40%] sm:w-[100%]">
+          <h1 className="font-bold text-4xl text-center p-4 text-accent">
+            Our Mission Statement
+          </h1>
+          <Mission />
         </div>
       </div>
     </div>
