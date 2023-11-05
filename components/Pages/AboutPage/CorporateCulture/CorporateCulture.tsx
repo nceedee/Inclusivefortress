88cb7fa95742bb9db7fc02349b49f8600615ed53
N Cee Dee <nnajidavid58@gmail.com>
2023-11-05 14:47:08 +0100

    sorted out about

--- components/Pages/AboutPage/CorporateCulture/CorporateCulture.tsx
+++ components/Pages/AboutPage/CorporateCulture/CorporateCulture.tsx
@@ -5,30 +5,28 @@ import Image from "next/image";
 
 export const CorporateCulture = () => {
   return (
-    <div className="flex w-full mb-11 items-center  p-8 bg-primary">
-      <div className="flex md:flex-row sm:flex-col items-center justify-evenly">
-        <div className="md:w-[30%]">
-          <Image src="/image/corprate.jpeg" alt="" width={400} height={300} />
-        </div>
-        <div className="md:w-[40%]" >
-          <Tabs
-            aria-label="Dynamic tabs"
-            items={tabs}
-            className="sm:w-full  m-auto"
-          >
-            {(item) => (
-              <Tab
-                key={item.id}
-                title={item.label}
-                className="flex items-center justify-center m-auto text-center"
-              >
-                <Card className="sm:w-full ">
-                  <CardBody>{item.content}</CardBody>
-                </Card>
-              </Tab>
-            )}
-          </Tabs>
-        </div>
+    <div className="flex mb-11 w-full sm:space-y-4 md:space-y-0 md:flex-row bg-primary sm:flex-col items-center justify-evenly">
+      <div className="md:w-[30%] sm:p-4 md:p-0 sm:w-[100%]">
+        <Image src="/image/corprate.jpeg" alt="" width={400} height={300} />
+      </div>
+      <div className="md:w-[40%] sm:w-[100%]">
+        <Tabs
+          aria-label="Dynamic tabs"
+          items={tabs}
+          className="sm:w-full  m-auto"
+        >
+          {(item) => (
+            <Tab
+              key={item.id}
+              title={item.label}
+              className="flex items-center justify-center m-auto text-center"
+            >
+              <Card className="sm:w-full ">
+                <CardBody>{item.content}</CardBody>
+              </Card>
+            </Tab>
+          )}
+        </Tabs>
       </div>
     </div>
   );
