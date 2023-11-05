e4892fe6da0f96797f050d3d53dd7f611f49533f
N Cee Dee <nnajidavid58@gmail.com>
2023-11-05 23:12:48 +0100

    updated

--- components/Pages/AboutPage/CorporateCulture/CorporateCulture.tsx
+++ components/Pages/AboutPage/CorporateCulture/CorporateCulture.tsx
@@ -7,7 +7,13 @@ export const CorporateCulture = () => {
   return (
     <div className="flex mb-11 w-full sm:space-y-4 md:space-y-0 md:flex-row bg-primary sm:flex-col items-center justify-evenly">
       <div className="md:w-[30%] sm:p-4 sm:w-[100%]">
-        <Image src="/image/corprate.jpeg" alt="" width={400} height={300} />
+        <Image
+          src="/image/Culture.jpg"
+          alt=""
+          width={400}
+          className="rounded object-fill w-full"
+          height={300}
+        />
       </div>
       <div className="md:w-[40%] sm:w-[100%]">
         <Tabs
