63f12064c7c885b007f730fbc9e6dbf3b0d2fe20
N Cee Dee <nnajidavid58@gmail.com>
2023-11-05 14:47:49 +0100

    sorted out about

--- components/Pages/AboutPage/CorporateCulture/CorporateCulture.tsx
+++ components/Pages/AboutPage/CorporateCulture/CorporateCulture.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export const CorporateCulture = () => {
   return (
     <div className="flex mb-11 w-full sm:space-y-4 md:space-y-0 md:flex-row bg-primary sm:flex-col items-center justify-evenly">
-      <div className="md:w-[30%] sm:p-4 md:p-0 sm:w-[100%]">
+      <div className="md:w-[30%] sm:p-4 sm:w-[100%]">
         <Image src="/image/corprate.jpeg" alt="" width={400} height={300} />
       </div>
       <div className="md:w-[40%] sm:w-[100%]">
